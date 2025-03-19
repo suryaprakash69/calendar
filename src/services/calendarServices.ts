2c28ef79de435d74a06a12e33d1113b973a98e6b
suryaprakash69 <suryamsp2020@gmail.com>
2025-03-19 17:50:54 +0530

    fix the ui

--- src/services/calendarServices.ts
+++ src/services/calendarServices.ts
@@ -54,7 +54,7 @@ const getCalendar = async (): Promise<EventData[]> => {
     },
     {
       id: 2,
-      summary: "Test",
+      summary: "1nd Round",
       desc: "1nd Round",
       start: "2025-03-19T20:00:00+05:30",
       end: "2025-03-19T21:00:00+05:30",
